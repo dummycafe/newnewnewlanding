6f8ab1a3598e24eb2da36979b67576b60877bb32
April <april@dummy.cafe>
2022-11-08 06:05:56 +0100

    use 1080p version if your screen isn't bigger than 1080p
    i could probably do this better (since it decides at page load and doesn't change) but i'm too lazy

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,7 +3,7 @@ window.onload = () => {
 	.then((response) => response.json())
 	.then((data) => {
         let selectedWallpaper = Math.floor(Math.random() * 7)
-		let imageUrl = data[selectedWallpaper].imageUrl;
+		let imageUrl = screen.height > 1080 ? data[selectedWallpaper].imageUrl : data[selectedWallpaper].fullUrl; 
         document.body.style.backgroundImage = "url('" + imageUrl + "')";
 
         let infoText = document.getElementById("wallpaper-info-text");
