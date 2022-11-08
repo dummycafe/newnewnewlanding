9cec71ba113ef6cb877aa5f6bef7cb4297588cc3
April <april@dummy.cafe>
2022-11-08 06:02:04 +0100

    random wallpaper from last 7d instead of just today's

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,12 @@ window.onload = () => {
     fetch('https://peapix.com/bing/feed?country=us')
 	.then((response) => response.json())
 	.then((data) => {
-		let imageUrl = data[0].imageUrl;
+        let selectedWallpaper = Math.floor(Math.random() * 7)
+		let imageUrl = data[selectedWallpaper].imageUrl;
         document.body.style.backgroundImage = "url('" + imageUrl + "')";
 
         let infoText = document.getElementById("wallpaper-info-text");
-        infoText.innerText = data[0].title;
-        infoText.href = data[0].pageUrl;
+        infoText.innerText = data[selectedWallpaper].title;
+        infoText.href = data[selectedWallpaper].pageUrl;
     }).catch((error) => console.error);
 }
